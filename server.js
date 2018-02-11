f6e4b79c60a90a3278e11f1fc061c5eed6d2dc91
jb <jbaptiste.daniel@gmail.com>
2018-02-11 14:57:04 +0100

    allow cross domain

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ var app = express();
 // set our application port
 app.set('port', 3000);
 
+//allow cross domain
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 // set morgan to log info about our requests for development use.
 app.use(morgan('dev'));
 
